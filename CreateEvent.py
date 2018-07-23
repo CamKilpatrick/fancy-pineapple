9b3bdbd2ae224aad8267f9fc65c46f30befd8c14
Christina Chance <christinachance315@gmail.com>
2018-07-23 12:51:21 -0400

    create event updated

--- CreateEvent.py
+++ CreateEvent.py
@@ -1,13 +1,18 @@
 import Event
 import webapp2
 
+import
+
 class NewEventHandler(webapp2.RequestHandler):
     def get(self):
         new_event = Event.Event()
-        new_event.title = 'Test Event'
-        new_event.description = 'this is a test event.'
-        new_event.tags = 'test, event'
-        print "done!"
+        new_event.eventname = self.request.get('eventname')
+        new_event.description = self.request.get('description')
+        new_event.tags = self.request.get('tags')
+        new_event.start= self.request.get('start')
+        new_event.end= self.request.get('end')
+        new_event.location= self.request.get('location')
+        print "Event Created!"
         new_event.put()
 
 app = webapp2.WSGIApplication([
