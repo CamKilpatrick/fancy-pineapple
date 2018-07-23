7e88e38d56e5f697c07fa3e14d7119d02acbbeaf
Cameron Kilpatrick <cameron.kilpatrick1@gmail.com>
2018-07-23 12:55:15 -0400

    more tweaks to EventDescription

--- EventDescription.py
+++ EventDescription.py
@@ -1,25 +1,21 @@
 import webapp2
 import jinja2
 import os
+import Event
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
 )
 
-
 class EventHandler(webapp2.RequestHandler):
     def get(self):
-        event_template = jinja_env.get_template('templates/ED.html')
+        specific_event = Event.query().fetch()
+        event_template = jinja_env.get_template('ED.html')
         html = event_template.render({
-        'event_title': 'My cool event',
-        'event_description': 'Cool stuff will be happening at this event',
-        'tags': 'fun, stuff'
-        # 'noun2': self.request.get('noun2'),
-        # 'presentVerb': self.request.get('presentVerb'),
-        # 'verb2': self.request.get('verb2'),
-        # 'name': self.request.get('name'),
-        # 'adjective': self.request.get('adjective'),
-        # 'pronoun': self.request.get('pronoun')
+        'event_title': specific_event.title,
+        'event_description': specific_event.description,
+        'tags': specific_event.tags
+###trying to retreive information from localhost:8000, need to run "Event.py" first ^^^
         })
         self.response.write(html)
 
