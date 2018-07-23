3faca3cca6790fea98f64a2c8c5ae07a605d8eaf
Ogenna Umeozulu <ogennac@gmail.com>
2018-07-23 16:46:50 -0400

    some suff by Ogenna

--- MainPage.py
+++ MainPage.py
@@ -3,17 +3,21 @@ import random
 import datetime
 import jinja2
 import os
+from FindEvents import FindEventsHandler
 
 jinja_env = jinja2.Environment(
     loader= jinja2.FileSystemLoader(os.path.dirname(__file__)),
 )
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-        main_template= jinja_env.get_template('MP.html')
-        html= main_template.render()
+        main_template = jinja_env.get_template('MP.html')
+        html = main_template.render()
         self.response.write(html)
 
 
-app= webapp2.WSGIApplication([
-    ('/', MainPageHandler)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPageHandler),
+    ('/find', FindEventsHandler),
 ],debug=True)
