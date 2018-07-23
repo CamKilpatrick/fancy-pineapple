8e2ff18e17680932f636ff35754c633837c72e00
Cameron Kilpatrick <cameron.kilpatrick1@gmail.com>
2018-07-23 12:30:43 -0400

    literally changed one word

--- MainPage.py
+++ MainPage.py
@@ -9,7 +9,7 @@ jinja_env = jinja2.Environment(
 )
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-        main_template= jinja_env.get_template('template/MP.html')
+        main_template= jinja_env.get_template('MP.html')
         html= main_template.render()
         self.response.write(html)
 
