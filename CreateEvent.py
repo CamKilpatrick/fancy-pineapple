037bada6b2e4256d426acc3188186412bb20cebe
Cameron Kilpatrick <cameron.kilpatrick1@gmail.com>
2018-07-23 11:53:56 -0400

    filled the empty ED.html with my code.

--- CreateEvent.py
+++ CreateEvent.py
@@ -1,14 +1,13 @@
 import Event
+import webapp2
 
 class NewEventHandler(webapp2.RequestHandler):
     def get(self):
         new_event = Event.Event()
-        event.title = 'Test Event'
-        event.description = 'this is a test event.'
-        event.tags = 'test, event'
-        })
+        new_event.title = 'Test Event'
+        new_event.description = 'this is a test event.'
+        new_event.tags = 'test, event'
         print "done!"
-        console.log("done!")
         new_event.put()
 
 app = webapp2.WSGIApplication([
