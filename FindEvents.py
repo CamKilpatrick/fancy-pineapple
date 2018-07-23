ee484021f4de9106cea2b55264f945d4e02c96a4
Ogenna Umeozulu <ogennac@gmail.com>
2018-07-23 12:47:07 -0400

    new stuff

--- FindEvents.py
+++ FindEvents.py
@@ -4,12 +4,26 @@ import os
 from google.appengine.ext import ndb
 import random
 import webapp2
+import Event
 
 import model
 
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
 
 class FindEventsHandler(webapp2.RequestHandler):
-    def get(self)
+    def get(self):
+        search = model.Search()
+        search.search_input = raw_input?????
+        search.put()
+        findevents_template = jinja_env.get_template('FE.html')
+        html = findevents_template.render()
+        #self.response.write(html) ****don't know if i need this.
+        #i got this from madlibs
+class Search(ndb.model):
+    def get(self):
+        search_input = ndb.StringProperty(required=True)
 
 
 
