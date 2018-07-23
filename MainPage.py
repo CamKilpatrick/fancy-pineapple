1bf38f900d073b0f09e99ebdf1f87a8a53442d92
Cameron Kilpatrick <cameron.kilpatrick1@gmail.com>
2018-07-23 12:02:56 -0400

    Copied Crisina's code intoMainPage and MP.

---
+++ MainPage.py
@@ -0,0 +1,19 @@
+import webapp2
+import random
+import datetime
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader= jinja2.FileSystemLoader(os.path.dirname(__file__)),
+)
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        main_template= jinja_env.get_template('template/MP.html')
+        html= main_template.render()
+        self.response.write(html)
+
+
+app= webapp2.WSGIApplication([
+    ('/', MainPageHandler)
+]debug=True)
