bc218d4a9c78d9775df519ed95b20fb056e7c275
Cameron Kilpatrick <cameron.kilpatrick1@gmail.com>
2018-07-24 15:10:21 -0400

    Moved CreateEvent into MainPage

--- MainPage.py
+++ MainPage.py
@@ -5,10 +5,10 @@ import jinja2
 import os
 from google.appengine.ext import ndb
 import Event
-from CreateEvent import EventTemplateHandler
 from login import LoginHandler
-from CreateEvent import NewEventHandler
 from SearchByName import SearchByName
+from DateTimeConverter import DateTimeConverter
+
 
 jinja_env = jinja2.Environment(
     loader= jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -35,6 +35,24 @@ class ActiveSearchHandler(webapp2.RequestHandler):
         self.response.write(search2.tags)
         self.response.write(search2.end)
         self.response.write(search2.location)
+
+class EventTemplateHandler(webapp2.RequestHandler):
+    def get(self):
+        createvent_template= jinja_env.get_template('CE.html')
+        html= createvent_template.render()
+        self.response.write(html)
+
+class NewEventHandler(webapp2.RequestHandler):
+    def get(self):
+        new_event = Event.Event()
+        print self.request.get('start')
+        new_event.eventname = self.request.get('eventname')
+        new_event.description = self.request.get('description')
+        new_event.tags = self.request.get('tags')
+        new_event.start = DateTimeConverter(self.request.get('start'))
+        new_event.end = DateTimeConverter(self.request.get('end'))
+        new_event.location = self.request.get('location')
+        new_event.put()
 
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
